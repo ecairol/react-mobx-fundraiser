ce36924e86e036735b3bd9884351f8827e480c3c
Esteban Cairol <ecairol@gmail.com>
2018-09-04 16:15:46 -0600

    QR Code to scan Bitcoin address

--- src/views/Home/ProjectCard/index.jsx
+++ src/views/Home/ProjectCard/index.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import QRCode from "qrcode.react";
 
 const ProjectCard = observer(({ project }) => (
   <div className="project-card">
     <div className="project-card__inner">
+      <div className="address-qr">
+        <h3>Donate</h3>
+        <QRCode value="{project.address}" level="M" />
+      </div>
       <h1>{project.title}</h1>
       <p>{project.description}</p>
       <p>
