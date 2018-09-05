fc2f204dffdb3dedcb1da285f8e444e3b4848ccc
Esteban Cairol <ecairol@gmail.com>
2018-09-04 22:32:09 -0600

    enforceActions to avoid changing state outside of an action.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import { configure } from "mobx";
 
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
+
+configure({
+  enforceActions: "observed"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
